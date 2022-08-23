a89fcddaa524b0b472c548945f1b7c62e76f451e
mostafizurhimself <mostafizurhimself@gmail.com>
2022-08-23 22:28:01 +0600

    Remove starpi

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'ce20602342ea3c9e26677cd0ab49b264'),
-    },
-  },
-});
--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'b899a8f0-b15f-42f0-a937-694267a96700'
-};
