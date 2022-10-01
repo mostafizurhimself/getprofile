dd6340cfe636432264e84a057fe5cc8fe1c4be4c
mostafizurhimself <mostafizurhimself@gmail.com>
2022-10-01 12:25:43 +0600

    Update hero section

--- pofology/src/components/partials/HeroSection.tsx
+++ pofology/src/components/partials/HeroSection.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { FiDribbble, FiFacebook, FiGithub, FiInstagram, FiTwitter } from 'react-icons/fi';
 import Button from '@/components/form/Button';
 import HeroBackground from './HeroBackground';
+import Typed from 'react-typed';
 
 const HeroSection = () => {
   return (
@@ -11,10 +12,19 @@ const HeroSection = () => {
       <HeroBackground />
       <div className="flex flex-col items-center">
         <div className="overflow-hidden rounded-full">
-          <Image src="/images/avatar/man.png" width={150} height={150} alt="avatar" />
+          <Image src="/images/avatar/man.png" width={180} height={180} alt="avatar" />
         </div>
         <h1 className="mt-4 text-3xl font-bold ">Mostafizur Rahman</h1>
-        <div className="mt-2 flex gap-1">I'm a Fullstack Developer</div>
+        <div className="mt-2 flex gap-1">
+          {' '}
+          I'm a{' '}
+          <Typed
+            strings={['Fullstack Developer', 'UI/UX Designer', 'Mobile App Developer']}
+            typeSpeed={60}
+            backSpeed={30}
+            loop
+          />
+        </div>
         <div className="mt-4  flex gap-5">
           <Link href="https://www.instragram.com">
             <a className="text-gray-700 transition-colors duration-150 hover:text-primary-500">
